8100ff9f3bb86c7b518f1bf2a47683ecf9f7c429
Mihail <misho_90@gbg.bg>
2013-11-03 00:13:09 +0200

    Iztriti greshni tablici

--- ESchool/src/main/java/mmm/eschool/Model/User.java
+++ ESchool/src/main/java/mmm/eschool/Model/User.java
@@ -1,21 +1,10 @@
 package mmm.eschool.Model;
 
 import java.io.Serializable;
-import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -23,40 +12,8 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @Table(schema = "eschool", name = "users")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
-    @NamedQuery(name = "User.findById", query = "SELECT u FROM User u WHERE u.id = :id"),
-    @NamedQuery(name = "User.findByUsername", query = "SELECT u FROM User u WHERE u.username = :username"),
-    @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")})
 public class User implements Serializable
 {
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "id")
-    private int id;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 20)
-    @Column(name = "username")
-    private String username;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 30)
-    @Column(name = "password")
-    private String password;
-    @ManyToMany(mappedBy = "userCollection")
-    private Collection<mmm.eschool.Model.Roles> rolesCollection;
-    @ManyToMany(mappedBy = "userCollection")
-    private Collection<Rights> rightsCollection;
-    @OneToMany(mappedBy = "userid")
-    private Collection<Parents> parentsCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userid")
-    private Collection<Students> studentsCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userid")
-    private Collection<Teachers> teachersCollection;
-   
   public enum Roles 
   { 
     ADMINISTRATOR("Администратор"), PARENT("Родител"), CLASSMASTER("Класен ръководител"), TEACHER("Учител"), STUDENT("Ученик"); 
@@ -66,6 +23,11 @@ public class User implements Serializable
     public String getRoleName() { return roleName; }
   }
   
+  @Id
+  @Column(length = 20)
+  private String username;
+  @Column(nullable = false, length = 50)
+  private String password;
   @Column(name = "user_role", nullable = false, length = 20)
   private String userRole;
  
@@ -98,75 +60,4 @@ public class User implements Serializable
   {
     this.userRole = userRole;
   }
-
-    public User() {
-    }
-
-    public User(int id) {
-        this.id = id;
-    }
-
-    public User(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-       @XmlTransient
-    public Collection<mmm.eschool.Model.Roles> getRolesCollection() {
-        return rolesCollection;
-    }
-
-    public void setRolesCollection(Collection<mmm.eschool.Model.Roles> rolesCollection) {
-        this.rolesCollection = rolesCollection;
-    }
-
-    @XmlTransient
-    public Collection<Rights> getRightsCollection() {
-        return rightsCollection;
-    }
-
-    public void setRightsCollection(Collection<Rights> rightsCollection) {
-        this.rightsCollection = rightsCollection;
-    }
-
-    @XmlTransient
-    public Collection<Parents> getParentsCollection() {
-        return parentsCollection;
-    }
-
-    public void setParentsCollection(Collection<Parents> parentsCollection) {
-        this.parentsCollection = parentsCollection;
-    }
-
-    @XmlTransient
-    public Collection<Students> getStudentsCollection() {
-        return studentsCollection;
-    }
-
-    public void setStudentsCollection(Collection<Students> studentsCollection) {
-        this.studentsCollection = studentsCollection;
-    }
-
-    @XmlTransient
-    public Collection<Teachers> getTeachersCollection() {
-        return teachersCollection;
-    }
-
-    public void setTeachersCollection(Collection<Teachers> teachersCollection) {
-        this.teachersCollection = teachersCollection;
-    }
-
-    @Override
-    public String toString() {
-        return "mmm.eschool.Model.User[ id=" + id + " ]";
-    }
 }
