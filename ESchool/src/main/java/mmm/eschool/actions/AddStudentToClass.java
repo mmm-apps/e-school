962f28fd69aebc00380ac7ebd9c43db10cd3a985
mdenev <miro.denev@gmail.com>
2013-12-13 14:14:16 +0200

    changest to add student to class with a problem

--- ESchool/src/main/java/mmm/eschool/actions/AddStudentToClass.java
+++ ESchool/src/main/java/mmm/eschool/actions/AddStudentToClass.java
@@ -12,137 +12,129 @@ import java.util.Map;
 import mmm.eschool.AnException;
 import mmm.eschool.model.Classes;
 import mmm.eschool.model.Student;
+import mmm.eschool.model.TeacherSubjects;
 import mmm.eschool.model.managers.ClassManager;
 import mmm.eschool.model.managers.StudentManager;
+import mmm.eschool.model.managers.TeacherSubjectsManager;
 import org.apache.struts2.interceptor.SessionAware;
 
 /**
  *
  * @author MMihov
  */
-public class AddStudentToClass extends ActionSupport implements SessionAware
-{
-  private Map session;
-  private String student;
-  private ArrayList<String> studentsList = new ArrayList<String>();
-  private String classNo;
-  private ArrayList<String> classesList = new ArrayList<String>();
+public class AddStudentToClass extends ActionSupport implements SessionAware {
 
-  private StudentManager studMan = new StudentManager();
-  private ClassManager classMan = new ClassManager();
-  private List<Student> studlist = studMan.getEntityList();
-  private List<Classes> classList = classMan.getEntityList();
+    private Map session;
+    private String student;
+    private ArrayList<String> studentsList = new ArrayList<String>();
+    private String classNo;
+    private ArrayList<String> classesList = new ArrayList<String>();
 
-  @Override
-  public void setSession(Map<String, Object> map)
-  {
-    this.session = map;
-  }
+    private StudentManager studMan = new StudentManager();
+    private ClassManager classMan = new ClassManager();
+    private List<Student> studlist = studMan.getEntityList();
+    private List<Classes> classList = classMan.getEntityList();
 
-  @Override
-  public String execute()
-  {
-    return null;
-  }
+    private TeacherSubjectsManager teacherSubjMgr = new TeacherSubjectsManager();
 
-  public String display()
-  {
-    for (Student displayedStudent : studlist)
-    {
-      String displayedClass;
-      if (displayedStudent.getClassId() == null)
-        displayedClass = "НЯМА КЛАС";
-      else
-        displayedClass = displayedStudent.getClassId().getClassName();
-      studentsList.add(displayedStudent.getFirstName() + " " + displayedStudent.getLastName()+ "  " + displayedClass);
+    @Override
+    public void setSession(Map<String, Object> map) {
+        this.session = map;
     }
 
-    for (Classes currentClass : classList)
-      classesList.add(currentClass.getClassName());
-    return SUCCESS;
-  }
-
-  public String addStudent()
-  {
-    Student newStudent = new Student();
-    Classes newClass = new Classes();
-
-    for (Student currentStudent : studlist)
-    {
-      String className;
-      if (currentStudent.getClassId() == null)
-        className = "НЯМА КЛАС";
-      else
-        className = currentStudent.getClassId().getClassName();
-      if (student.equals(currentStudent.getFirstName() + " " + currentStudent.getLastName() + "  " + className))
-      {
-        newStudent = currentStudent;
-        break;
-      }
+    @Override
+    public String execute() {
+        return null;
     }
 
-    for (Classes currentClass : classList)
-    {
-      if (classNo.equals(currentClass.getClassName()))
-      {
-        newClass = currentClass;
-        break;
-      }
+    public String display() {
+        for (Student displayedStudent : studlist) {
+            String displayedClass;
+            if (displayedStudent.getClassId() == null) {
+                displayedClass = "НЯМА КЛАС";
+            } else {
+                displayedClass = displayedStudent.getClassId().getClassName();
+            }
+            studentsList.add(displayedStudent.getFirstName() + " " + displayedStudent.getLastName() + " " + displayedClass + " " + displayedStudent.getEmail());
+        }
+
+        for (Classes currentClass : classList) {
+            classesList.add(currentClass.getClassName());
+        }
+        return SUCCESS;
     }
 
-    try
-    {
-      newClass.getStudentList().add(newStudent);
-      newStudent.setClassId(newClass);
-      classMan.update(newClass);
-      studMan.update(newStudent);
-      addFieldError("student", "Student recorded succesufully!");
-      return SUCCESS;
+    public String addStudent() throws AnException {
+        Student newStudent = new Student();
+        Classes clas = classMan.getClassByName(classNo);
+        for (Student s : studMan.getEntityList()) {
+            String className;
+            if (s.getClassId() == null) {
+                className = "НЯМА КЛАС";
+            } else {
+                className = s.getClassId().getClassName();
+            }
+
+            if ((s.getFirstName() + " " + s.getLastName() + " " + className + " " + s.getEmail()).equals(student)) {
+                newStudent = s;
+                break;
+            }
+        }
+
+        clas.getStudentList().add(newStudent);
+        newStudent.setClassId(clas);
+        classMan.update(clas);
+        studMan.update(newStudent);
+
+        if (!newStudent.getSubjectsSet().isEmpty())
+        {
+            newStudent.getSubjectsSet().clear();
+            studMan.update(newStudent);
+        }
+        for (TeacherSubjects ts : teacherSubjMgr.getEntityList())
+        {
+            if (ts.getClasses().getClassName().equals(classNo)) 
+            {
+                newStudent.getSubjectsSet().add(ts.getSubject());
+                ts.getSubject().getStudentsSet().add(newStudent);
+                teacherSubjMgr.update(ts);
+            }
+        }
+
+        studMan.update(newStudent);
+      
+        return SUCCESS;
     }
-    catch (AnException ex)
-    {
-      ex.printStackTrace();
+
+    public String getStudent() {
+        return student;
     }
-    return ERROR;
-  }
 
-  public String getStudent()
-  {
-    return student;
-  }
+    public ArrayList<String> getStudentsList() {
+        return studentsList;
+    }
 
-  public ArrayList<String> getStudentsList()
-  {
-    return studentsList;
-  }
+    public String getClassNo() {
+        return classNo;
+    }
 
-  public String getClassNo()
-  {
-    return classNo;
-  }
+    public ArrayList<String> getClassesList() {
+        return classesList;
+    }
 
-  public ArrayList<String> getClassesList()
-  {
-    return classesList;
-  }
+    public void setStudent(String student) {
+        this.student = student;
+    }
 
-  public void setStudent(String student)
-  {
-    this.student = student;
-  }
+    public void setStudentsList(ArrayList<String> studentsList) {
+        this.studentsList = studentsList;
+    }
 
-  public void setStudentsList(ArrayList<String> studentsList)
-  {
-    this.studentsList = studentsList;
-  }
+    public void setClassNo(String _class) {
+        this.classNo = _class;
+    }
 
-  public void setClassNo(String _class)
-  {
-    this.classNo = _class;
-  }
-
-  public void setClassesList(ArrayList<String> classesList)
-  {
-    this.classesList = classesList;
-  }
+    public void setClassesList(ArrayList<String> classesList) {
+        this.classesList = classesList;
+    }
 }
